1f33f01a710682da4239e7a670a2d5da7133bb15
QinSu-a <seigakufight@sina.cn>
2019-10-16 16:58:51 +0800

    Signed-off-by: QinSu-a <seigakufight@sina.cn>

---
+++ src/test/java/tut4/CalcTest.java
@@ -0,0 +1,27 @@
+package tut4;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+class CalcTest {
+	private Calc calc;
+	@BeforeEach
+	void setUp() throws Exception {
+		calc = new Calc();
+	}
+
+	@Test
+	void testAdd() {
+		int a=1;
+		int b=1;
+		int result=calc.add(a,b);
+		assertEquals(result,2);
+	}
+
+
+
+}
